406c531ca867ced241e7a559227b8aed62d720d1
Lionel Alves <alves.lionel5@gmail.com>
2015-02-09 00:59:08 +0100

    First commit

---
+++ FirstbornTests/FirstbornTests.m
@@ -0,0 +1,40 @@
+//
+//  FirstbornTests.m
+//  FirstbornTests
+//
+//  Created by Lionel on 06/02/2015.
+//  Copyright (c) 2015 Lionel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface FirstbornTests : XCTestCase
+
+@end
+
+@implementation FirstbornTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '7.0'
+inhibit_all_warnings!
+
+link_with 'Firstborn', 'FirstbornTests'
+pod 'CHTCollectionViewWaterfallLayout'
+pod 'AFNetworking'
